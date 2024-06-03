0f7c0b72e4d8ebdc2f1b15a23b9e0b6d5732c815
Andrea De Bortoli <debortolia@ynap.world>
2024-06-03 23:05:11 +0200

    Add LenghtValidator and relative Tests

---
+++ src/Validator/LenghtValidator.cs
@@ -0,0 +1,32 @@
+﻿namespace Validator
+{
+    public class LenghtValidator : IValidator
+    {
+        public Response Validate(string password)
+        {
+            if (password is null)
+            {
+                return new Response
+                {
+                    IsValid = false,
+                    Message = "Password cannot be null"
+                };
+            }
+
+            if (password.Length < 8)
+            {
+                return new Response()
+                {
+                    IsValid = false,
+                    Message = "Password must be at least 8 characters"
+                };
+            }
+
+            return new Response()
+            {
+                IsValid = true,
+                Message = string.Empty
+            };
+        }
+    }
+}
